783c97ab7c1551c02baec23e8e2420f2aa15dadd
Jackson Hong <jacksonxxhong@gmail.com>
2014-08-25 00:09:09 -0400

    Create new DichotomyLogic class. This will house the game logic.

---
+++ Dichotomy/DichotomyLogic.swift
@@ -0,0 +1,13 @@
+//
+//  DichotomyLogic.swift
+//  Dichotomy
+//
+//  Created by Jackson Hong on 2014-08-25.
+//  Copyright (c) 2014 Jackson Hong. All rights reserved.
+//
+
+import SpriteKit
+
+class DichotomyLogic {
+    
+}
